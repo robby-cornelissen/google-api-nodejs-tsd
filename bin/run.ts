dfb959c8d20ae03b4230b5506681cfaffe77b68d
procrastinatos <robby.cornelissen@gmail.com>
2016-02-20 18:45:29 +0900

    modelled REST parameters

--- bin/run.ts
+++ bin/run.ts
@@ -5,9 +5,13 @@ import {Generator} from "../lib/generator";
 let generator = new Generator();
 generator.generate().then(() => console.log('Done')).catch((e) => console.error(e));
 
-//import * as google from "googleapis";
-//
+import * as google from "googleapis";
+
 //let r = google.discovery('v1').apis.list({}, (e, d) => {
 //    console.log(e);
 //    console.log(d.discoveryVersion);
 //});
+
+google.discovery('v1').apis.list({}, (error, data) => {
+    console.log(data.discoveryVersion);
+})
