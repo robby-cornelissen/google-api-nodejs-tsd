268271081f294ac5d14299e569d83d8b6bc7ad58
procrastinatos <robby.cornelissen@gmail.com>
2016-02-20 11:10:54 +0900

    baselined before nunjucks refactoring

--- typings/main.d.ts
+++ typings/main.d.ts
@@ -2,5 +2,6 @@
 /// <reference path="main/ambient/google-auth-library/google-auth-library.d.ts" />
 /// <reference path="main/ambient/mkdirp/mkdirp.d.ts" />
 /// <reference path="main/ambient/node/node.d.ts" />
+/// <reference path="main/ambient/nunjucks/nunjucks.d.ts" />
 /// <reference path="main/ambient/request/request.d.ts" />
 /// <reference path="main/ambient/swig/swig.d.ts" />
---
+++ typings/main/ambient/nunjucks/nunjucks.d.ts
@@ -0,0 +1,31 @@
+declare module "nunjucks" {
+    export function render(name: string, context: any, callback?: (e: Error, result: any) => any): any;
+
+    export function renderString(string: string, context: any, callback?: (e: Error, result: any) => any): any;
+
+    export function compile(string: string, environment?: any, path?: string): any;
+
+    export function configure(path?: string, options?: {
+        autoescape?: boolean;
+        throwOnUndefined?: boolean;
+        trimBlocks?: boolean;
+        lstripBlocks?: boolean;
+        watch?: boolean;
+        noCache?: boolean;
+        web?: {
+            useCache?: boolean;
+            async?: boolean;
+        };
+        express?: any;
+        tags?: {
+            blockStart?: string;
+            blockEnd?: string;
+            variableStart?: string;
+            variableEnd?: string;
+            commentStart?: string;
+            commentEnd?: string;
+        }
+    });
+
+    export function installJinjaCompat(): void;
+}
