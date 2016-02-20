854eef1c9dafc4fbb4ba33ef9631d56e1628d399
procrastinatos <robby.cornelissen@gmail.com>
2016-02-20 21:24:47 +0900

    added very basic test

--- bin/test.ts
+++ bin/test.ts
@@ -2,3 +2,12 @@
 
 import * as google from "googleapis";
 
+google.discovery('v1').apis.list({
+    preferred: true
+}, (e, data) => {
+    if(e) {
+        console.error(e);
+    } else {
+        console.log(data.items.map(item => item.name + ' ' + item.version).join('\n'));
+    }
+});
